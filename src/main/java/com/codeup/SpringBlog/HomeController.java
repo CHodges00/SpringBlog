07c97d2c8a71da658b8d93e0715614fdb6248b42
Cody Hodges <hodges.cody00@gmail.com>
2022-04-18 14:53:51 -0500

    Spring continued

--- src/main/java/com/codeup/SpringBlog/HomeController.java
+++ src/main/java/com/codeup/SpringBlog/HomeController.java
@@ -2,20 +2,12 @@ package com.codeup.SpringBlog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    //    ON VISIT TO /landing = RUN METHOD LANDING()
-    @GetMapping("/landing")
-    @ResponseBody
-    public String landing(){
-        return "This is the landing page!";
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
     }
-
-
-
-
-
 }
