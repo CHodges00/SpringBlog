3b7a23a9e2fe60ec0f32e9caedd68a71e8b27731
Cody Hodges <hodges.cody00@gmail.com>
2022-04-18 14:58:59 -0500

    Spring continued

--- src/main/java/com/codeup/SpringBlog/HelloController.java
+++ src/main/java/com/codeup/SpringBlog/HelloController.java
@@ -1,6 +1,7 @@
 package com.codeup.SpringBlog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -29,8 +30,23 @@ public class HelloController {
     }
 
 
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
 
 
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
 
 
 }
