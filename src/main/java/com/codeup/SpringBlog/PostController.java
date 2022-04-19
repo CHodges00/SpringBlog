30e759387d341fe4a3194fa9c008d35fbbb87e8d
Cody Hodges <hodges.cody00@gmail.com>
2022-04-19 14:34:22 -0500

    Spring

--- src/main/java/com/codeup/SpringBlog/PostController.java
+++ src/main/java/com/codeup/SpringBlog/PostController.java
@@ -1,37 +1,48 @@
 package com.codeup.SpringBlog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
-    @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
+
+    //    MAKE INDEX THAT WILL SHOW ALL POSTS
+    @GetMapping("/posts")
     public String postsIndex() {
-        return "Posts index page";
+
+        return "posts/index";
     }
 
-    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String individualPost() {
-        return "View individual post";
-    }
-
-
-    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
-    @ResponseBody
-    public String viewForm() {
-        return "View form for creating post";
-    }
-
-
-    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
-    @ResponseBody
-    public String createPost(){return "Create new post";}
-    {
-
+    //    MAKE SHOW THAT WILL SHOW INDIVIDUAL POSTS
+//    WHEN POST IS PICKED FROM VIEW (give href with integer in .html), REDIRECT TO POSTS/SHOW, THEN display post info on .html
+    @GetMapping( "/posts/{post}")
+    public String postsShow(@PathVariable String post, Model model) {
+        model.addAttribute("post", post);
+        model.addAttribute("post1", new Post(
+                "Individual Post Title",
+                "individual Post Body")
+        );
+        return "posts/show";
     }
 }
+
+
+//    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String viewForm() {
+//        return "View form for creating post";
+//    }
+//
+//
+//    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+//    @ResponseBody
+//    public String createPost(){return "Create new post";}
+//    {
+
+//    }
+
